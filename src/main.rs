b94af08d159543eb71e267460a479cb56534e509
Angelo Rendina <angelo.rendina@prima.it>
2024-12-02 09:23:54 +0100

    with timings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 mod day01;
 
 fn main() {
-    println!("01.1: {}", day01::star_one());
-    println!("01.2: {}", day01::star_two());
+    println!("01.1: {}", solve_timed(day01::star_one));
+    println!("01.2: {}", solve_timed(day01::star_two));
+}
+
+fn solve_timed<F, O>(f: F) -> String
+where
+    F: Fn() -> O,
+    O: std::fmt::Display,
+{
+    let now = std::time::SystemTime::now();
+    let result = f();
+    let elapsed = now.elapsed().unwrap();
+    format!("{result} in {elapsed:?}")
 }
